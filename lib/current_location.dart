f232b7975c1d0df46cca32052d9ad67b7b81f8f7
Saurabh Maurya <saurabhmaurya2050@gmail.com>
2024-08-18 23:05:43 +0530

    nearby proto completed

--- lib/current_location.dart
+++ lib/current_location.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:spark/nearby_list.dart';
+import 'package:spark/searchScreen.dart';
 
 class CurrentLocation extends StatefulWidget {
   const CurrentLocation({super.key});
@@ -11,8 +15,8 @@ class CurrentLocation extends StatefulWidget {
 }
 
 class _CurrentLocationState extends State<CurrentLocation> {
+    
   final Completer<GoogleMapController> _controller = Completer();
-
   static const _kGooglePlex = CameraPosition(
     target: LatLng(28.375724999658214, 79.45794690859752),
     zoom: 14,
@@ -57,13 +61,33 @@ class _CurrentLocationState extends State<CurrentLocation> {
 
   @override
   Widget build(BuildContext context) {
+  final Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _kGooglePlex,
-        markers: Set<Marker>.of(_markers),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      body: Stack(
+        children: [
+          SearchScreen(),
+          GoogleMap(
+            initialCameraPosition: _kGooglePlex,
+            zoomControlsEnabled: false,
+            markers: Set<Marker>.of(_markers),
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+       
+          Positioned(
+           
+            bottom: 0,
+            left: 0,
+            right: 0,
+          
+            child: NearbyList(
+
+            ),
+          ),
+
+          
+        ],
       ),
     );
   }
